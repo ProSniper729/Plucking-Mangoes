fb9e3818dc616fe589a5959aca199dda7df61456
ProSniper729 <76519132+ProSniper729@users.noreply.github.com>
2021-04-18 11:39:55 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,35 @@
+class Launcher{
+    constructor(bodyA,pointB){
+        var options = {
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.003,
+            length:10
+        }
+        this.pointB = pointB;
+        this.launcherObject = Constraint.create(options);
+        World.add(world,this.launcherObject);
+
+    }
+
+    fly(){
+        this.launcherObject.bodyA = null;
+    }
+
+    attach(){
+        this.launcherObject.bodyA = stoneObj.body;
+    }
+
+    display(){
+      if(this.launcherObject.bodyA){
+        var pointA = this.launcherObject.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+
+      }
+    }
+
+
+
+}
